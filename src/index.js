714edc44ead2d9e305f0f3686b4d8e9cf0f4abf1
hms <hm@hms.bzh>
2019-01-24 09:22:04 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,118 @@
+const {
+  BaseKonnector,
+  requestFactory,
+  signin,
+  scrape,
+  saveBills,
+  log
+} = require('cozy-konnector-libs')
+const request = requestFactory()
+
+const baseUrl = 'https://connect.garmin.com'
+
+const j = request.jar()
+request = requestFactory({
+  cheerio: true,
+  json: false,
+  // debug: true,
+  jar: j
+});
+
+module.exports = new BaseKonnector(start)
+
+// The start function is run by the BaseKonnector instance only when it got all the account
+// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
+// the account information come from ./konnector-dev-config.json file
+async function start(fields) {
+  log('info', 'Authenticating ...')
+  await authenticate(fields.login, fields.password)
+  log('info', 'Successfully logged in')
+  // The BaseKonnector instance expects a Promise as return of the function
+  log('info', 'Fetching the list of documents')
+  const $ = await request(`${baseUrl}/index.html`)
+  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
+  log('info', 'Parsing list of documents')
+  const documents = await parseDocuments($)
+
+  // here we use the saveBills function even if what we fetch are not bills, but this is the most
+  // common case in connectors
+  log('info', 'Saving data to Cozy')
+  await saveBills(documents, fields, {
+    // this is a bank identifier which will be used to link bills to bank operations. These
+    // identifiers should be at least a word found in the title of a bank operation related to this
+    // bill. It is not case sensitive.
+    identifiers: ['books']
+  })
+}
+
+// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
+// even if this in another domain here, but it works as an example
+function authenticate(username, password) {
+  
+  const arrParams = {
+    'service': 'https://connect.garmin.com/modern/',
+    'webhost': 'https://connect.garmin.com',
+    'source': 'https://connect.garmin.com/en-US/signin',
+    'clientId': 'GarminConnect',
+    'gauthHost': 'https://sso.garmin.com/sso',
+    'consumeServiceTicket': 'false'
+  };
+  
+  return request({
+    url: `https://sso.garmin.com/sso/login#`,
+    method: "GET",
+    params: {}
+  });
+
+}
+
+// The goal of this function is to parse a html page wrapped by a cheerio instance
+// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
+function parseDocuments($) {
+  // you can find documentation about the scrape function here :
+  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
+  const docs = scrape(
+    $,
+    {
+      title: {
+        sel: 'h3 a',
+        attr: 'title'
+      },
+      amount: {
+        sel: '.price_color',
+        parse: normalizePrice
+      },
+      fileurl: {
+        sel: 'img',
+        attr: 'src',
+        parse: src => `${baseUrl}/${src}`
+      },
+      filename: {
+        sel: 'h3 a',
+        attr: 'title',
+        parse: title => `${title}.jpg`
+      }
+    },
+    'article'
+  )
+  return docs.map(doc => ({
+    ...doc,
+    // the saveBills function needs a date field
+    // even if it is a little artificial here (these are not real bills)
+    date: new Date(),
+    currency: '€',
+    vendor: 'template',
+    metadata: {
+      // it can be interesting that we add the date of import. This is not mandatory but may be
+      // useful for debugging or data migration
+      importDate: new Date(),
+      // document version, useful for migration after change of document structure
+      version: 1
+    }
+  }))
+}
+
+// convert a price string to a float
+function normalizePrice(price) {
+  return parseFloat(price.replace('£', '').trim())
+}
