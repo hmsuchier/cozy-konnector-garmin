ccbecf458a9e92ca316f94bd466e10d8b5387aa9
hms <hm@hms.bzh>
2019-01-27 16:08:16 +0100

    removed log

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-const {
-  BaseKonnector,
-  requestFactory,
-
-  scrape,
-  saveBills,
-  log
-} = require('cozy-konnector-libs')
+const { BaseKonnector, requestFactory, log } = require('cozy-konnector-libs')
 const request = requestFactory()
 
 const baseUrl = 'https://connect.garmin.com/modern/'
 
 const j = request.jar()
 
-let ticket = null;
+let ticket = null
 
 module.exports = new BaseKonnector(start)
 
@@ -22,7 +15,7 @@ async function start(fields) {
   log('info', 'Successfully logged in')
   // The BaseKonnector instance expects a Promise as return of the function
   log('info', 'Fetching the list of activities')
-    
+
   const activities = await parseActivities(0, 10)
 
   let activity = activities[0]
@@ -31,15 +24,14 @@ async function start(fields) {
 
   const details = await parseActivityDetails(activity.activityId)
 
-  log("info", summary)
-  log("info", details)
+  log('info', summary)
+  log('info', details)
 }
 
 // this shows authentication using the [signin function]
 //(https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
 // even if this in another domain here, but it works as an example
 async function authenticate(username, password) {
-  
   //parameters for sso pre-start
   const arrParams = {
     service: 'https://connect.garmin.com/activities/',
@@ -48,123 +40,136 @@ async function authenticate(username, password) {
     clientId: 'GarminConnect',
     gauthHost: 'https://sso.garmin.com/sso',
     consumeServiceTicket: 'false',
-    redirectAfterAccountLoginUrl:	'https://connect.garmin.com/modern/activities',
-    redirectAfterAccountCreationUrl: 'https://connect.garmin.com/modern/activities',
+    redirectAfterAccountLoginUrl:
+      'https://connect.garmin.com/modern/activities',
+    redirectAfterAccountCreationUrl:
+      'https://connect.garmin.com/modern/activities',
     locale: 'fr_FR',
     id: 'gauth-widget'
   }
 
-    const arrData = {
-        "username": username,
-        "password": password,
-        "embed": "false",
-    }
+  const arrData = {
+    username: username,
+    password: password,
+    embed: 'false'
+  }
 
   //sso pre start
-  return request.get({
-    url: `https://sso.garmin.com/sso/login#`,
-    qs: arrParams,
-    jar: j
-    }).then( x => {
-       //actual login phase
-      return request.post({
-        url: `https://sso.garmin.com/sso/signin`,
-        qs: arrParams,
-        form: arrData,
-        cheerio: false,
-        jar: j
-
-      }).then(x => {
-        let reg= /\?ticket=([^\"]+)\"/gm  
+  return request
+    .get({
+      url: `https://sso.garmin.com/sso/login#`,
+      qs: arrParams,
+      jar: j
+    })
+    .then(() => {
+      //actual login phase
+      return request
+        .post({
+          url: `https://sso.garmin.com/sso/signin`,
+          qs: arrParams,
+          form: arrData,
+          cheerio: false,
+          jar: j
+        })
+        .then(x => {
+          let reg = /\?ticket=([^"]+)"/gm
           ticket = reg.exec(x)[1]
 
-          return request.post({
-            url: baseUrl,
-            qs: {
-              "ticket": ticket
-            },
-            simple:false,
-            resolveWithFullResponse: true ,
-            followAllRedirects: false,
-            jar: j
-          }).then(x => {
-            if (x.statusCode === 302) {
-              //catch redirect
-              redirectUrl = x.rawHeaders[(x.rawHeaders.indexOf('Location')+1)]
-              return request.get({
-                url: redirectUrl,
-                resolveWithFullResponse: true ,
-                followAllRedirects: true,
-                jar: j
-              }).then(x => {
-                log("info", "OK!!")
-                return true
-              }).catch (e => {
-                return false
-              })
-            }
-          }).catch(e => {
-            log("warn", e)
-            return false
-          })
-      }) 
+          return request
+            .post({
+              url: baseUrl,
+              qs: {
+                ticket: ticket
+              },
+              simple: false,
+              resolveWithFullResponse: true,
+              followAllRedirects: false,
+              jar: j
+            })
+            .then(x => {
+              if (x.statusCode === 302) {
+                //catch redirect
+                const redirectUrl =
+                  x.rawHeaders[x.rawHeaders.indexOf('Location') + 1]
+                return request
+                  .get({
+                    url: redirectUrl,
+                    resolveWithFullResponse: true,
+                    followAllRedirects: true,
+                    jar: j
+                  })
+                  .then(() => {
+                    log('info', 'OK!!')
+                    return true
+                  })
+                  .catch(e => {
+                    log('warn', e)
+                    return false
+                  })
+              }
+            })
+            .catch(e => {
+              log('warn', e)
+              return false
+            })
+        })
     })
-    .catch(
-      e => log("warn", "SSO pre-start error")
-    )
+    .catch(e => log('warn', 'SSO pre-start error', e))
 }
 
 /**
- * 
- * @param {*} start 
- * @param {*} limit 
+ *
+ * @param {*} start
+ * @param {*} limit
  */
 function parseActivities(start, limit) {
-
-  return request.get({
-    url: baseUrl + 'proxy/activitylist-service/activities/search/activities',
-    qs: {
-      start: start,
-      limit: limit
-    },
-    jar: j
-  }).then(x => {
-    return x
-  }).catch(e => {
-    log("warn", "list was not collected")
-    return null
-  })
+  return request
+    .get({
+      url: baseUrl + 'proxy/activitylist-service/activities/search/activities',
+      qs: {
+        start: start,
+        limit: limit
+      },
+      jar: j
+    })
+    .then(x => {
+      return x
+    })
+    .catch(e => {
+      log('warn', 'list was not collected', e)
+      return null
+    })
 }
 
 //get activity summary
 function parseActivitySummary(activityId) {
-
-  return request.get({
-    url: baseUrl + 'proxy/activity-service/activity/' + activityId,
-    jar: j
-  }).then(x => {
-    return x
-  }).catch(e => {
-    log("warn", "activity was not collected", e)
-    return null
-  })
+  return request
+    .get({
+      url: baseUrl + 'proxy/activity-service/activity/' + activityId,
+      jar: j
+    })
+    .then(x => {
+      return x
+    })
+    .catch(e => {
+      log('warn', 'activity was not collected', e)
+      return null
+    })
 }
 
 //get activity details
 function parseActivityDetails(activityId) {
-
-  return request.get({
-    url: baseUrl + "proxy/activity-service/activity/" + activityId + '/details',
-    jar: j
-  }).then(x => {
-    return x
-  }).catch(e => {
-    log("warn", "activity detail was not collected", e)
-    return null
-  })
-}
-
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+  return request
+    .get({
+      url:
+        baseUrl + 'proxy/activity-service/activity/' + activityId + '/details',
+      jar: j
+    })
+    .then(x => {
+      return x
+    })
+    .catch(e => {
+      log('warn', 'activity detail was not collected', e)
+      return null
+    })
 }
